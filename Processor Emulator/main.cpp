6ddf6a53a28c85aab4011baf1273559b1d58f729
Shadience <rusibatullin08@mail.ru>
2023-03-04 14:50:27 +0400

    Added "Hello, World! image" + Fullscreen mode

--- Processor Emulator/main.cpp
+++ Processor Emulator/main.cpp
@@ -1,10 +1,22 @@
 #include "stdafx.h"
 using namespace std;
 
-// Game window constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+// Screen resolution initializing
+const int SCREEN_WIDTH = GetSystemMetrics(SM_CXSCREEN);
+const int SCREEN_HEIGHT = GetSystemMetrics(SM_CYSCREEN);
 
+void gameClose(SDL_Window* window, vector<SDL_Surface*> surfaces)
+{
+	//Deallocate all surfaces
+	for (int i = 0; i < surfaces.size(); i++)
+	{
+		SDL_FreeSurface(surfaces[i]);
+	}
+	// Destroy game window
+	SDL_DestroyWindow(window);
+	//Quit SDL
+	SDL_Quit();
+}
 
 int main(int argc, char** argv)
 {
@@ -18,10 +30,12 @@ int main(int argc, char** argv)
 	// Game window creation
 	SDL_Window* window = NULL;
 	// Surface in the game window creation
-	SDL_Surface * screenSurface = NULL;
+	SDL_Surface* screenSurface = NULL;
+	// The image we will load and show on the screen
+	SDL_Surface* HelloWorld = NULL;
 
-	// Game window initialization
-	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	// Game window fullscreen initialization
+	window = SDL_CreateWindow("Processor Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN);
 	// If game window not initialize throw error
 	if (window == NULL)
 	{
@@ -32,16 +46,22 @@ int main(int argc, char** argv)
 	screenSurface = SDL_GetWindowSurface(window);
 	// Surface fill
 	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-	// Surface update
+
+	// Create hello world image
+	HelloWorld = SDL_LoadBMP("Resources/Sprites/hello_world.bmp");
+	if (HelloWorld == NULL)
+	{
+		cout << SDL_GetError() << endl;
+		exit(-1);
+	}
+	// Main surface create
+	SDL_BlitSurface(HelloWorld, NULL, screenSurface, NULL);
+	// Update window surfaces
 	SDL_UpdateWindowSurface(window);
 
 	// Game window cycle
 	SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
 
-	// Destroy game window
-	SDL_DestroyWindow(window);
-	//Quit SDL
-	SDL_Quit();
-
+	gameClose(window, { screenSurface, HelloWorld });
 	return 0;
 }
