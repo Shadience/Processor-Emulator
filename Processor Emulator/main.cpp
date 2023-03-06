b04c5935d8c44d3965525e2f513e3af6bd0d228d
Shadience <rusibatullin08@mail.ru>
2023-03-06 22:26:16 +0400

    Small main update

--- Processor Emulator/main.cpp
+++ Processor Emulator/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 const int SCREEN_WIDTH = GetSystemMetrics(SM_CXSCREEN);
 const int SCREEN_HEIGHT = GetSystemMetrics(SM_CYSCREEN);
 
+// Game closing function
 void gameClose(SDL_Window* window, vector<SDL_Surface*> surfaces)
 {
 	//Deallocate all surfaces
@@ -35,7 +36,7 @@ int main(int argc, char** argv)
 	SDL_Surface* HelloWorld = NULL;
 
 	// Game window fullscreen initialization
-	window = SDL_CreateWindow("Processor Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN);
+	window = SDL_CreateWindow("Processor Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN);
 	// If game window not initialize throw error
 	if (window == NULL)
 	{
@@ -60,7 +61,15 @@ int main(int argc, char** argv)
 	SDL_UpdateWindowSurface(window);
 
 	// Game window cycle
-	SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+	SDL_Event e;
+	bool quit = false;
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT) quit = true;
+		}
+	}
 
 	gameClose(window, { screenSurface, HelloWorld });
 	return 0;
